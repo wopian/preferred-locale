192012e50d7be5d2630c88234f863252e3746743
James Harris <wopian@wopian.me>
2020-07-14 01:10:12 +0100

    fix(userLocales): check Intl is implemented before checking if DateTimeFormat is implemented

--- packages/preferred-locale/src/utils/userLocales/index.js
+++ packages/preferred-locale/src/utils/userLocales/index.js
@@ -6,6 +6,6 @@
  */
 export const userLocales = fallback => {
   if (global?.navigator?.languages) return navigator.languages.concat([ fallback ])
-  if ('DateTimeFormat' in Intl) return [ Intl.DateTimeFormat().resolvedOptions().locale, fallback ]
+  if (Intl && 'DateTimeFormat' in Intl) return [ Intl.DateTimeFormat().resolvedOptions().locale, fallback ]
   return [ fallback ]
 }
